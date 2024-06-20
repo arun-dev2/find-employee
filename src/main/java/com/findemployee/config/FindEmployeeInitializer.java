252dc3a514f5b031904791ad6fcf26340bd1f565
arunp <arunp@DESKTOP-62D4RUP>
2024-06-20 20:02:58 +0530

    refactored dispatcher servlet preparing method.

--- src/main/java/com/findemployee/config/FindEmployeeInitializer.java
+++ src/main/java/com/findemployee/config/FindEmployeeInitializer.java
@@ -1,27 +1,26 @@
 package com.findemployee.config;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
-
-public class FindEmployeeInitializer implements WebApplicationInitializer {
+public class FindEmployeeInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
 	@Override
-	public void onStartup(ServletContext servletContext) throws ServletException {
-
-		AnnotationConfigWebApplicationContext webAppContext = new AnnotationConfigWebApplicationContext();
-
-		webAppContext.register(FindEmployeeConfiguration.class);
-
-		DispatcherServlet ds = new DispatcherServlet(webAppContext);
-
-		ServletRegistration.Dynamic customDs = servletContext.addServlet("femp", ds);
-		customDs.setLoadOnStartup(1);
-		customDs.addMapping("/femp.com/*");
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
 	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		Class arr[] = {FindEmployeeConfiguration.class};
+		return arr;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		String arr[] = {"/femp.com/*"};
+		return arr;
+	}
+
 
 }
