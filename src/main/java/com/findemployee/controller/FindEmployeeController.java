1c8d11c6fa36c09127d67ea18bb3f8889c787505
arunp <arunp@DESKTOP-62D4RUP>
2024-06-21 20:49:02 +0530

    using modelattribute and spring mvc form tag for data binding.

--- src/main/java/com/findemployee/controller/FindEmployeeController.java
+++ src/main/java/com/findemployee/controller/FindEmployeeController.java
@@ -1,7 +1,7 @@
 package com.findemployee.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.findemployee.dto.UserInfoDTO;
@@ -10,13 +10,12 @@ import com.findemployee.dto.UserInfoDTO;
 public class FindEmployeeController {
 
 	@RequestMapping("/")
-	public String welcome() {
+	public String welcome(@ModelAttribute("userInfo") UserInfoDTO userInfoDTO) {
 		return "Welcome";
 	}
 
 	@RequestMapping("/process")
-	public String processResult(UserInfoDTO userInfoDTO, Model model) {
-		model.addAttribute(userInfoDTO);
+	public String processResult(@ModelAttribute("userInfo") UserInfoDTO userInfoDTO) {
 		return "Result";
 	}
 }
--- src/main/webapp/WEB-INF/view/Result.jsp
+++ src/main/webapp/WEB-INF/view/Result.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>  
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
 <!DOCTYPE html>
@@ -9,7 +10,9 @@
 <body>
 	<h1 align=center>Find Employee</h1>
 	<hr />
+	<form:form modelAttribute = "userInfo">
 	<p>Result</P>
-	<p>Candidate ${userInfoDTO.candidateName} profile matches for the ${userInfoDTO.roleName} role.</p>
+	<p>Candidate ${userInfo.candidateName} profile matches for the ${userInfo.roleName} role.</p>
+	</form:form>
 </body>
 </html>
